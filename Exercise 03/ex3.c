45cdf9cfbd5b594a208825c07fbe4b8987242261
Kevin Stock <kevinstock@tantalic.com>
2014-04-03 22:38:42 -0700

    Adds exercise 3: "Formatted printing"

---
+++ Exercise 03/ex3.c
@@ -0,0 +1,16 @@
+/**
+ * Learn C the Hard Way
+ * Exercise 3: Formatted printing
+ */
+
+#include <stdio.h>
+
+int main() {
+	int age = 10;
+	int height = 72;
+	
+	printf("I am %d years old.\n", age);
+	printf("I am %d inches tall.\n", height);
+	
+	return 0;
+}
