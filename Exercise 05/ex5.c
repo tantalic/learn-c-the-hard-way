c30a403ab4222c223c8f8191b120218c4bd28f0f
Kevin Stock <kevinstock@tantalic.com>
2014-04-05 18:22:36 -0700

    Adds exercise 5: "The Structure of a C Program"

---
+++ Exercise 05/ex5.c
@@ -0,0 +1,19 @@
+/**
+ * Learn C the Hard Way
+ * Exercise 5: The Structure of a C Program
+ *
+ * Kevin Stock
+ * April 5, 2014
+ */
+
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char*argv[]) {
+	int distance = 100;
+	
+	// this is also a comment
+	printf("You are %d miles away.\n", distance);
+	
+	return 0;
+}
