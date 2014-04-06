76af0175cd66c0cff463215825380d3d3c5f5ad6
Kevin Stock <kevinstock@tantalic.com>
2014-04-05 18:16:01 -0700

    Adds exercise 4: "Introducing Valgrind"

---
+++ Exercise 04/ex4.1.c
@@ -0,0 +1,16 @@
+/**
+ * Learn C the Hard Way
+ * Exercise 4: Valgrind
+ *
+ * Extra Credit - Fixed by using Valrdind and Clang Compiler Warnings
+ */
+
+#include <stdio.h>
+
+int main() {
+	int age = 10;
+	int height = 41;
+	
+	printf("I am %d years old.\n", age);
+	printf("I am %d in ches tall.\n", height);
+}
---
+++ Exercise 04/ex4.c
@@ -0,0 +1,14 @@
+/**
+ * Learn C the Hard Way
+ * Exercise 4: Valgrind
+ */
+
+/* WARNING: This program is wrong on purpose. */
+
+int main() {
+	int age = 10;
+	int height;
+	
+	printf("I am %d years old.\n");
+	printf("I am %d in ches tall.\n", height);
+}
